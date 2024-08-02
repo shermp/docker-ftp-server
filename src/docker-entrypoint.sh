2638d914ba3d1e65abafc9611361fe3fc8f71aff
Sherman Perry <14854761+shermp@users.noreply.github.com>
2024-08-03 09:24:26 +1200

    Group changes

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
-addgroup \
-	-g $GID \
-	-S \
-	$FTP_USER
+GRP_NAME=$FTP_USER
+if getent group $GID; then
+	GRP_NAME=$(getent group $GID | cut -d: -f1)
+else
+	addgroup \
+		-g $GID \
+		-S \
+		$GRP_NAME
+fi
 
 adduser \
 	-D \
-	-G $FTP_USER \
+	-G $GRP_NAME \
 	-h /home/$FTP_USER \
 	-s /bin/false \
 	-u $UID \
 	$FTP_USER
 
-mkdir -p /home/$FTP_USER
-chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+#mkdir -p /home/$FTP_USER
+#chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
 echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
 
 touch /var/log/vsftpd.log
